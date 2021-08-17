61c8da1fb9e1f29023d6eda9348e52a2bd24d09b
Lernni <Lernni@users.noreply.github.com>
2021-08-17 10:44:44 +0200

    reactive browser timline

--- src/frontend/src/views/Stats/Browser.vue
+++ src/frontend/src/views/Stats/Browser.vue
@@ -5,7 +5,7 @@
         <h3>Zeitraum</h3>
         <b-row>
           <b-col cols="7">
-            <apexchart type="rangeBar" height="150px" :options="chartOptions" :series="series"></apexchart>
+            <apexchart ref="timeline" type="rangeBar" height="150px" :options="chartOptions" :series="series"></apexchart>
           </b-col>
           <b-col>
             <b-row>
@@ -13,18 +13,18 @@
                 <label for="start-datepicker">Startdatum</label>
                 <b-form-datepicker
                   id="start-datepicker"
-                  v-model="form.startDate"
+                  v-model="startDate"
                   class="mb-2"
                   locale="de-DE"
                   :start-weekday="1"
-                  :max="form.endDate"
+                  :max="endDate"
                   labelHelp=""
                   labelNoDateSelected="Kein Datum ausgewählt"
                 ></b-form-datepicker>
               </b-col>
               <b-col>
                 <label for="start-timepicker">Startzeit</label>
-                <b-form-input id="input" v-model="form.startTime" type="time" class="mb-2"></b-form-input>
+                <b-form-input id="input" v-model="startTime" type="time" class="mb-2"></b-form-input>
               </b-col>
             </b-row>
             <b-row>
@@ -32,11 +32,11 @@
                 <label for="end-datepicker">Enddatum</label>
                 <b-form-datepicker
                   id="end-datepicker"
-                  v-model="form.endDate"
+                  v-model="endDate"
                   class="mb-2"
                   locale="de-DE"
                   :start-weekday="1"
-                  :min="form.startDate"
+                  :min="startDate"
                   :max="maxDate"
                   labelHelp=""
                   labelNoDateSelected="Kein Datum ausgewählt"
@@ -44,7 +44,7 @@
               </b-col>
               <b-col>
                 <label for="end-timepicker">Endzeit</label>
-                <b-form-input id="end-timepicker" v-model="form.endTime" type="time" class="mb-2"></b-form-input>
+                <b-form-input id="end-timepicker" v-model="endTime" type="time" class="mb-2"></b-form-input>
               </b-col>
             </b-row>
           </b-col>
@@ -58,8 +58,13 @@
 <script>
 import VueApexCharts from 'vue-apexcharts'
 
-const currentDate = new Date()
-const currentTime = currentDate.getHours() + ":" +  currentDate.getMinutes()
+var de = require("apexcharts/dist/locales/de.json")
+
+const Formatter = {
+  toTime: function(dateTime) {
+    return ("0" + new Date(dateTime).getHours()).slice(-2) + ":" + ("0" + new Date(dateTime).getMinutes()).slice(-2)
+  }
+}
 
 export default {
   name: "Browser",
@@ -69,13 +74,12 @@ export default {
   data() {
     return {
 
-      maxDate: currentDate,
+      maxDate: new Date(),
+      enableZoomEvents: true,
 
-      form: {
-        startDate: currentDate,
-        endDate: currentDate,
-        startTime: currentTime,
-        endTime: currentTime
+      dateTimeRange: {
+        min: Date.now(),
+        max: Date.now(),
       },
 
       // timeline
@@ -116,6 +120,8 @@ export default {
       ],
       chartOptions: {
         chart: {
+          locales: [de],
+          defaultLocale: "de",
           height: "300px",
           type: 'rangeBar',
           toolbar: {
@@ -125,12 +131,25 @@ export default {
           },
           events: {
             // eslint-disable-next-line no-unused-vars
-            beforeZoom: function(chartContext, axis) {
-              console.log(axis.xaxis.min)
-              this.form.startDate = new Date(axis.xaxis.min)
-              this.form.endDate = new Date(axis.xaxis.max)
-              this.form.startTime = this.form.startDate.getHours() + ":" + this.form.startDate.getMinutes()
-              this.form.endTime = this.form.endDate.getHours() + ":" + this.form.endDate.getMinutes()
+            zoomed: function(chartContext, axis) {
+              this.updateDateTimeRange(axis.xaxis)
+            }.bind(this),
+
+            // eslint-disable-next-line no-unused-vars
+           /* beforeResetZoom: function(chartContext, options) {
+              console.log(options)
+              this.updateDateTimeRange(options.config.xaxis)
+            }.bind(this),*/
+
+            // eslint-disable-next-line no-unused-vars
+            scrolled: function(chartContext, axis) {
+              this.updateDateTimeRange(axis.xaxis)
+            }.bind(this),
+
+            // eslint-disable-next-line no-unused-vars
+            dataPointSelection: function(event, chartContext, config) {
+              var dataPointRange = this.series[0].data[config.dataPointIndex].y
+              this.$refs.timeline.zoomX(dataPointRange[0], dataPointRange[1])
             }.bind(this)
           }
         },
@@ -140,12 +159,101 @@ export default {
           }
         },
         xaxis: {
-          type: 'datetime'
+          type: 'datetime',
+          min: undefined,
+          max: undefined,
         },
         yaxis: {
           show: false
         }
       },
+    }
+  },
+
+  mounted() {
+    this.$refs.timeline.zoomX(this.dateTimeRange.min, this.dateTimeRange.max)
+  },
+  computed: {
+    startDate: {
+      get: function() {
+        return new Date(this.dateTimeRange.min)
+      },
+      set: function(date) {
+        date = new Date(date)
+        var startDateTime = new Date(this.dateTimeRange.min)
+
+        console.log(date)
+        console.log(date.getYear())
+        startDateTime.setFullYear(date.getFullYear())
+        startDateTime.setMonth(date.getMonth())
+        startDateTime.setDate(date.getDate())
+
+        this.dateTimeRange.min = startDateTime.getTime()
+      }
+    },
+
+    startTime: {
+      get: function() {
+        return Formatter.toTime(this.dateTimeRange.min)
+      },
+      set: function(time) {
+        var splittedTime = time.split(':')
+        var startDateTime = new Date(this.dateTimeRange.min)
+
+        startDateTime.setHours(splittedTime[0])
+        startDateTime.setMinutes(splittedTime[1])
+
+        this.dateTimeRange.min = startDateTime.getTime()
+      }
+    },
+
+    endDate: {
+      get: function() {
+        return new Date(this.dateTimeRange.max)
+      },
+      set: function(date) {
+        date = new Date(date)
+        var endDateTime = new Date(this.dateTimeRange.max)
+
+        endDateTime.setFullYear(date.getFullYear())
+        endDateTime.setMonth(date.getMonth())
+        endDateTime.setDate(date.getDate())
+
+        this.dateTimeRange.max = endDateTime.getTime()
+      }
+    },
+
+    endTime: {
+      get: function() {
+        return Formatter.toTime(this.dateTimeRange.max)
+      },
+      set: function(time) {
+        var splittedTime = time.split(':')
+        var endDateTime = new Date(this.dateTimeRange.max)
+
+        endDateTime.setHours(splittedTime[0])
+        endDateTime.setMinutes(splittedTime[1])
+
+        this.dateTimeRange.max = endDateTime.getTime()
+      }
+    }
+  },
+  watch: {
+    dateTimeRange: {
+      // eslint-disable-next-line no-unused-vars
+      handler: function(newVal, oldVal) {
+        this.enableZoomEvents = false
+        this.$refs.timeline.zoomX(newVal.min, newVal.max)
+        this.enableZoomEvents = true
+      },
+      deep: true
+    }
+  },
+  methods: {
+    updateDateTimeRange(range) {
+      if (this.enableZoomEvents) {
+        this.dateTimeRange = range
+      }
     }
   }
 }
