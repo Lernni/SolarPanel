33138924646c880528f21a2a7897ced799127e09
Lernni <Lernni@users.noreply.github.com>
2021-10-14 18:36:52 +0000

    fix wrong web_socket url  in production mode

--- src/frontend/src/scripts/socketInstance.js
+++ src/frontend/src/scripts/socketInstance.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
 
-export default io(process.env.VUE_APP_WEB_SOCKET_URL, {
+const web_socket_url = (process.env.NODE_ENV == 'production') ? "" : "ws://localhost:4000"
+
+export default io(web_socket_url, {
   auth: {
     token: localStorage.getItem('token')
   }
