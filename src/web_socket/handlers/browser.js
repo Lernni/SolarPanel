5f1b8008fff530dcd7fa0e7fb6c02b92d34a2f1d
Lernni <Lernni@users.noreply.github.com>
2022-04-13 13:20:20 +0200

    revert: addition to revert commit 776ec42
    * revert timeline handling in web socket

--- src/web_socket/handlers/browser.js
+++ src/web_socket/handlers/browser.js
@@ -19,20 +19,4 @@ module.exports = (io, socket) => {
       console.log(error)
     })
   })
-  
-  socket.on("getDBSections", ({start_time, end_time}, callback) => {
-    axios.get("/db/sections", {
-      params: {
-        start_time, end_time
-      }
-    })
-    .then((response) => {
-      callback({
-        data: response.data
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-  })
 }
