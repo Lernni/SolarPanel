e98da1ba2bcdea5521df3f24b4f61a8999eb181d
Lernni <Lernni@users.noreply.github.com>
2022-05-06 16:33:47 +0000

    chore: add update script

---
+++ src/local_client/update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+RESET=0
+START=0
+
+# check option parameters
+while getopts rs flag
+do
+  case "${flag}" in
+    r) RESET=1 ;;
+    s) START=1 ;;
+  esac
+done
+
+# shutdown current docker instance
+cd ~/SolarPanel/src/
+docker-compose down
+
+# update repo if requested
+if [ $RESET -eq 1 ]; then
+  git reset --hard origin/main
+  git fetch
+  git pull
+fi
+
+# get all other parameters except options
+declare -a finalopts
+finalopts=()
+
+for o in "$@"; do
+  if [[ $o == "-"* ]] ; then
+    continue
+  fi
+  finalopts+=("$o")
+done
+
+# rebuild all docker containers or only those that are given as parameters
+docker-compose build --build-arg CACHEBUST=$(date +%s) "${finalopts[@]}"
+
+# restart SolarPanel if requested
+if [ $START -eq 1 ]; then
+  sudo ./local_client/start_solarpanel.sh
+fi
